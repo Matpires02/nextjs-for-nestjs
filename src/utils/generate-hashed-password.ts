32c0f0735f4226ffceeb1148fef49b9cb27356f0
Matheus Pires <61023277+Matpires02@users.noreply.github.com>
2026-07-06 15:41:16 -0300

    aula 265 - Criação de action para criação de usuário

--- src/utils/generate-hashed-password.ts
+++ src/utils/generate-hashed-password.ts
@@ -1,7 +1,7 @@
-import { hashPassword } from "@/lib/login/manage-login";
+import { hashPassword } from '@/lib/login/manage-login';
 
 (async () => {
-  const suaSenha = "";
+  const suaSenha = '';
   const hashDaSenhaEmBase64 = await hashPassword(suaSenha);
   console.log({ hashDaSenhaEmBase64 });
 })();
